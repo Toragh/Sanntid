f20c26bddac0813b22248ff3a7bca606658757a0
succoDiPompelmo <zenari12@gmail.com>
2019-01-21 19:08:52 +0100

    Add task 2

---
+++ Ex1/Part4/RESULT.md
@@ -0,0 +1,1 @@
+Som oftest er det et uregelmessig forhold på hvilke tråder som leser/skriver til den felles variabelen i.  
--- Ex1/Part4/c/foo.c
+++ Ex1/Part4/c/foo.c
@@ -5,24 +5,30 @@ int i = 0;
 
 // Note the return type: void*
 void* incrementingThreadFunction(){
-    // TODO: increment i 1_000_000 times
+    for(int c = 0; c < 10000000; c++) {
+       i++;
+    }
     return NULL;
 }
 
 void* decrementingThreadFunction(){
-    // TODO: decrement i 1_000_000 times
+    for(int c = 0; c < 10000000; c++) {
+       i--;
+    }
     return NULL;
 }
 
 
 int main(){
-    // TODO: declare incrementingThread and decrementingThread (hint: google pthread_create)
+    printf("Start");
+    pthread_t incrementingThread, decrementingThread;
+
     pthread_create(&incrementingThread, NULL, incrementingThreadFunction, NULL);
     pthread_create(&decrementingThread, NULL, decrementingThreadFunction, NULL);
-    
+
     pthread_join(incrementingThread, NULL);
     pthread_join(decrementingThread, NULL);
-    
+
     printf("The magic number is: %d\n", i);
     return 0;
 }
