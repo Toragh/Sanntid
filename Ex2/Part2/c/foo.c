d28d5895b37d5d8a671fa122a226309bd1ac395e
succoDiPompelmo <viljarn@stud.ntnu.no>
2019-01-28 19:03:49 +0100

    Added ex3 and documentation

--- Ex2/Part2/c/foo.c
+++ Ex2/Part2/c/foo.c
@@ -2,27 +2,39 @@
 #include <stdio.h>
 
 int i = 0;
-
+pthread_mutex_t lock;
 // Note the return type: void*
 void* incrementingThreadFunction(){
-    // TODO: increment i 1_000_000 times
+    for(int c = 0; c < 10000000; c++) {
+      pthread_mutex_lock(&lock);
+       i++;
+       pthread_mutex_unlock(&lock);
+    }
     return NULL;
 }
 
 void* decrementingThreadFunction(){
-    // TODO: decrement i 1_000_000 times
+    for(int c = 0; c < 10000000; c++) {
+      pthread_mutex_lock(&lock);
+       i--;
+       pthread_mutex_unlock(&lock);
+    }
     return NULL;
 }
 
 
 int main(){
-    // TODO: declare incrementingThread and decrementingThread (hint: google pthread_create)
+    printf("Start");
+    pthread_mutex_init(&lock, NULL);
+
+    pthread_t incrementingThread, decrementingThread;
+
     pthread_create(&incrementingThread, NULL, incrementingThreadFunction, NULL);
     pthread_create(&decrementingThread, NULL, decrementingThreadFunction, NULL);
-    
+
     pthread_join(incrementingThread, NULL);
     pthread_join(decrementingThread, NULL);
-    
+    pthread_mutex_destroy(&lock);
     printf("The magic number is: %d\n", i);
     return 0;
 }
